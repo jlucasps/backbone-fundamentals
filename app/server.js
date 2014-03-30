1c34e35ad2fc831cdff4900d9728c586f16b1c07
João Lucas <jlucasps@gmail.com>
2014-03-30 19:57:42 -0300

    Add CRUD api

--- app/server.js
+++ app/server.js
@@ -40,10 +40,14 @@ app.listen( port, function() {
 
 mongoose.connect('mongodb://localhost/library_database');
 
+var Keywords = new mongoose.Schema({
+    keyword: String
+});
 var Book = new mongoose.Schema({
     title: String,
     author: String,
-    releaseDate: Date
+    releaseDate: Date,
+    keywords: [ Keywords ]
 });
 
 var BookModel = mongoose.model('Book', Book);
@@ -62,7 +66,8 @@ app.post('/api/books', function(request, response) {
     var book = new BookModel({
         title: request.body.title,
         author: request.body.author,
-        releaseDate: request.body.releaseDate
+        releaseDate: request.body.releaseDate,
+        keywords: request.body.keywords
     });
     book.save(function(err) {
         if(!err) {
@@ -91,11 +96,26 @@ app.put('/api/books/:id', function(request, response) {
         book.title = request.body.title;
         book.author = request.body.author;
         book.releaseDate = request.body.releaseDate;
+        book.keywords = request.body.keywords;
 
         return book.save( function( err ) {
             if(!err) {
                 console.log('book updated');
                 return response.send(book);
+            } else {
+                console.log(err);
+            }
+        });
+    });
+});
+
+app.delete('/api/books/:id', function(request, response) {
+    console.log( 'Deleting book with id: ' + request.params.id );
+    return BookModel.findById(request.params.id, function(err, book) {
+        return book.remove( function(err) {
+            if (!err) {
+                console.log('book removed');
+                return response.send('');
             } else {
                 console.log(err);
             }
