cb6f2076dfff74c52af109c106fd7499b75c53b6
João Lucas <jlucasps@gmail.com>
2014-03-30 19:40:18 -0300

    Create and read books

--- app/server.js
+++ app/server.js
@@ -36,3 +36,51 @@ var port = 4711;
 app.listen( port, function() {
     console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
 });
+
+
+mongoose.connect('mongodb://localhost/library_database');
+
+var Book = new mongoose.Schema({
+    title: String,
+    author: String,
+    releaseDate: Date
+});
+
+var BookModel = mongoose.model('Book', Book);
+
+app.get('/api/books', function(request, response) {
+    return BookModel.find( function(err, books) {
+        if (!err) {
+            return response.send(books);
+        } else {
+            return console.log(err);
+        }
+    });
+});
+
+app.post('/api/books', function(request, response) {
+    var book = new BookModel({
+        title: request.body.title,
+        author: request.body.author,
+        releaseDate: request.body.releaseDate
+    });
+    book.save(function(err) {
+        if(!err) {
+            console.log('created');
+            return response.send(book)
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+app.get('/api/books/:id', function(request, response) {
+    return BookModel.findById(request.params.id, function(err, book) {
+        if(!err) {
+            return response.send(book);
+        } else {
+            return console.log(err);
+        }
+    });
+});
